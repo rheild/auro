713e3114732df41f4043efb70feb98a3fd21ab55
rheild <115514011+rheild@users.noreply.github.com>
2022-10-14 22:13:59 -0400

    completing base64 (wip)

--- libs/base64.js
+++ libs/base64.js
@@ -4,12 +4,29 @@ Copyright 2022, rheild (https://github.com/rheild)
 Do not remove this notice.
 */
 
-(function(){
+const base64 = (input) => {
+    // to binary function
     const toBinary = (char) => {
         let binary = (char.charCodeAt('0') >>> 0).toString(2);
-        for (let i = binary.length; i < 8; i++) {
+        while (binary.length < Math.ceil(binary.length / 8) * 8) {
             binary = "0" + binary;
         }
         return binary;
     }
-})();
+    
+    // convert input to binary
+    let binary = '';
+    for (let i = 0; i < input.length; i++) {
+        binary += toBinary(input[i]);
+    }
+    console.log(binary);
+    
+    // convert binary to base64
+    for (let i = 0; i < Math.ceil(binary.length / 6) * 6; i += 6) {
+        // split into sextet
+        let temp = binary.slice(i, i + 6);
+        do () {
+        
+        }
+    }
+}
