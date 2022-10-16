6f8f7a4d156c46ed197337462092139f36572984
rheild <115514011+rheild@users.noreply.github.com>
2022-10-16 10:45:51 -0400

    optimizations to color.js

--- js/color.js
+++ js/color.js
@@ -1,32 +1,32 @@
 /*
-Features:
-- RGBA to HEX
-- HEX to RGBA
+- RGB to HEX
+- HEX to RGB
 
-Licensed under the GNU GPL v3.0 License. See https://raw.githubusercontent.com/rheild/auro/main/LICENSE for details.
+Licensed under the GNU GPL v3.0 License. See https://www.gnu.org/licenses/gpl-3.0.txt for details.
 */
 
 const Color = {
-    rgbaToHex(r, g, b, a) {
-        function toHex(rgba) {
-            return Math.round(rgba).toString(16);
+    rgbToHex(r, g, b) {
+        function toHex(rgb) {
+            let temp = String(Math.floor(rgb).toString(16));
+            if (temp.length === 1) temp = '0' + temp;
+            return temp;
         }
         
-        return toHex(r) + toHex(g) + toHex(b) + toHex(a);
+        return toHex(r) + toHex(g) + toHex(b);
     },
     
-    hexToRgba(hex) {
-        function toRgba(v) {
-            let temp = parseInt(hex[v] + hex[v+1], 16);
-            if (isNaN(temp)) temp = undefined;
+    hexToRgb(hex) {
+        function toRgb(v) {
+            let temp = parseInt(hex[v] + hex[v + 1], 16);
+            if (isNaN(temp)) temp = 0;
             return temp;
         }
         
         return {
-            r: toRgba(0),
-            g: toRgba(2),
-            b: toRgba(4),
-            a: toRgba(6),
+            r: toRgb(0),
+            g: toRgb(2),
+            b: toRgb(4)
         }
     }
 }
